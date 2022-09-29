21a4aa7ea26dee608bb7ce3ab6c799e9c84bb75d
x213212 <x8795278@gmail.com>
2022-09-29 13:04:41 +0800

    Update user.c

--- user.c
+++ user.c
@@ -10,7 +10,7 @@ void user_task0(void)
 	int i=0;
 	while (1) {
 		lib_puts("Task0: Running...\n");
-		lib_delay(300);
+		lib_delay(10);
 		os_kernel();
 			for(int j= 0; j < 200 ;j++){
 			putpixel(i,j,0x7800);
@@ -28,16 +28,22 @@ void user_task1(void)
 	lib_puts("Task1: Now, return to kernel mode\n");
 	os_kernel();
 	int i=0;
+	int flag =0;
 	while (1) {
 		lib_puts("Task1: Running...\n");
 		lib_delay(300);
 		os_kernel();
-		
+		if(flag)
+		fillrect(20+i,i,0x000000);
+		else
 		fillrect(20+i,i,0x0000ff);
+
 		
 		i+=40;
-		if (i == 320)
+		if (i == 320){
 		i=0;
+		flag=~flag;
+		}
 	}
 }
 
